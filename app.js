9075d59eafde64639a223486013e30dc3bfcd708
ArnobKumarSaha <arnobkumarsaha00@gmail.com>
2021-11-02 23:38:16 +0600

    MongoDb connected !

--- app.js
+++ app.js
@@ -19,26 +19,52 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // -----------------------------------------------------------------------------------
 
+// Database related things 
+
+const mongoose = require("mongoose");
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+const MONGODB_URI = "mongodb+srv://arnobkumarsaha:sustcse16@cluster0.nj6lk.mongodb.net/swe?retryWrites=true&w=majority";
+
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+});
+
+app.use(
+  session({
+    httpOnly: false,
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
+);
+
+// -----------------------------------------------------------------------------------
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var errorController = require('./controllers/error')
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use(errorController.get404)
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
+mongoose.connect(
+    MONGODB_URI,
+    {
+      useUnifiedTopology: true,
+      useNewUrlParser: true
+    }
+  )
+  .then(result =>{
+    app.listen(3000);
+    console.log("MongoDB is connected !!      Listening on port 3000.");
+  })
+  .catch(err =>{
+    console.log(err);
+  });
+  
 module.exports = app;
